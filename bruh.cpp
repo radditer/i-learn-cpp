85201d55c9a19387f5ef7e661ccb3dac4429ce86
radditer <oliverleo2007@gmail.com>
2021-10-29 22:57:59 +0800

    drongon is bald

--- bruh.cpp
+++ bruh.cpp
@@ -7,5 +7,5 @@ int main()
   std::cout << "bruh\n";
   std::cout << "i have homework to do i shouldn't be doing this\n";
   std::cin >> x;
-  std::cout << "number is: " << x << " , but im sure you knew that\n";
+  std::cout << "number is: " << x << ", but im sure you knew that\n";
 }
